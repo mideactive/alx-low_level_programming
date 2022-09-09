87e34ab171559fadeb89b36b57d0822f76154a58
mideactive <jenyoolumide@yahoo.com>
2022-09-09 05:36:14 -0700

    NP

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,7 +17,9 @@ int main(void)
 		printf("%d is positive\n", n);
 	else if (n < 0)
 		printf("%d is nagative\n", n);
-	else
+	else if (n == 0)
 		printf("%d is zero\n", n);
+	else
+		printf("\n");
 	return (0);
 }
