dfa747da2ca092ea72ac5f409355170589b86408
mideactive <57058018+mideactive@users.noreply.github.com>
2022-09-13 21:23:55 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char c[8] = "_putchar";
+int i;
+for (i = 0; i < 9; i++)
+
+_putchar(c[i]);
+
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef PUTCHAR_h_
+#define PUTCHAR_h_
+
+/**
+ * main - check the code
+ *
+ * Return: _putchar and a newline
+ */
+int _putchar(char c);
+int _newline(char d);
+void print_alphabet(void);
+#endif
