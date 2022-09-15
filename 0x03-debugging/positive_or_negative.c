793cab9b84636bad255ca3dfc207d53e51be6445
mideactive <57058018+mideactive@users.noreply.github.com>
2022-09-15 23:27:39 +0000

    2 main

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "main.h"
 /**
- * main - Entry point
+ *numbers - positive or nagative numbers
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
-void positive_or_nagative(int n)
+void positive_or_negative(int i)
 {
 	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-        if (n > 0)
-                printf("%d is positive\n", n);
-        else if (n == 0)
-                printf("%d is zero\n", n);
-        else
-                printf("%d is negative\n", n);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 
-        return(n);
+	return (0);
 }
