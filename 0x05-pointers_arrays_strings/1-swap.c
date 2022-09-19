6581e7f436aca957040c84b3f1130a2e1835c40f
mideactive <57058018+mideactive@users.noreply.github.com>
2022-09-19 17:12:07 +0000

    value swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*swap_int - swaps values between int variables.
+*
+*@a: pointer to variable a.
+*
+*@b: pointer to variable b.
+*
+*Return: Nothing
+*/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
