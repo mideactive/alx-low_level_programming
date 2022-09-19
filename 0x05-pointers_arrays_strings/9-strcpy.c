7b6ecd61ecd8793410ddcf136d8c72d41a933172
mideactive <57058018+mideactive@users.noreply.github.com>
2022-09-19 19:58:04 +0000

    string pointer by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies a string pointed to by @src, including the
+ * @dest: A buffer to copy the string to.
+ * @src: The source string to copy.
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *aux = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	return (aux);
+}
