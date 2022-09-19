1a1902cc032b8515327fc13aeaa87b4298d9939d
mideactive <57058018+mideactive@users.noreply.github.com>
2022-09-19 19:43:48 +0000

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_array - Prints an inputted number of array
+ *@a: The array of integers.
+ *@n: The number of elements to be printed.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i == n - 1)
+			continue;
+
+		printf(", ");
+	}
+
+	printf("\n");
+}
