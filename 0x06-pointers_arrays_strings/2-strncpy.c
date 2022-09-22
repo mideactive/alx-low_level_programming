ab761dddfbc8f24e530f7aebf1b3a4c132cf883d
mideactive <57058018+mideactive@users.noreply.github.com>
2022-09-22 11:23:03 +0000

    copying from one string to another

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strncpy - a function that copies from one string to another
+*@dest: The destination variable
+*@src: The source variable
+*@n: an input integer
+*Return: Always 0
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
+	srclen++;
+
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	return (temp);
+}
