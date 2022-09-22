adde1153f8140edbeb631e187e7e787fa136548d
mideactive <57058018+mideactive@users.noreply.github.com>
2022-09-22 10:55:33 +0000

    concatonating two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include "main.h"
+/**
+*_strncat - concatonates two strings
+*@dest: pointer to the first string
+*@src: pointer to the second string
+*@n: number of bytes to copy from src
+*Return: Always 0
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
+	while (*dest)
+		dest++;
+
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
+}
