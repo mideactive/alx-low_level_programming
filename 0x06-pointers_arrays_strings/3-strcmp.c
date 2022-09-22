16be329ca21dd0f2f394b06b7a1e914b328ae5d7
mideactive <57058018+mideactive@users.noreply.github.com>
2022-09-22 17:43:15 +0000

    reverse array

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_strcmp - compares two strings
+*@s1: first string to be compared
+*@s2: second string to be compared
+*Return: difference s1 & s2
+*/
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++, s2++;
+	}
+	return (*s1 - *s2);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <string.h>
+
 int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+
 
 
 #endif
