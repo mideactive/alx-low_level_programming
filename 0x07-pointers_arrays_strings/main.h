8cddb498a606c1ef6852c940555cb03c70482fe7
mideactive <57058018+mideactive@users.noreply.github.com>
2022-09-26 17:41:47 +0000

    chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 int _putchar(char a);
 char *_memset(char *s, char b, unsigned int n);
@@ -11,7 +12,7 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+void print_chessboard(char (*a)[8]);
 
 
 #endif
