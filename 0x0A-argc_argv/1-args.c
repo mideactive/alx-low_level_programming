ce343e0112481a01cf6c930af87326ea56b9e164
mideactive <57058018+mideactive@users.noreply.github.com>
2022-09-30 11:09:06 +0000

    printing a single value

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*main - holds counter and pointer to arguments
+*@argc: Count number of supplied arguments
+*@argv: Points to the supplied string or arguments
+*Return: Always zero
+*/
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+		return (0);
+}
