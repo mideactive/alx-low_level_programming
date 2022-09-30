a0d6ad0a343a5caa0ed7571a5a962b2ce96ff92e
mideactive <57058018+mideactive@users.noreply.github.com>
2022-09-30 14:17:29 +0000

    add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+*main - Adds two numbers
+*@argc: Holds number count
+*@argv: Holds string arrray
+*Return: Always 0
+*/
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int num, digit, sum = 0;
+
+	for (num = 1; num < argc; num++)
+	{
+		for (digit = 0; argv[num][digit]; digit++)
+		{
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[num]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
