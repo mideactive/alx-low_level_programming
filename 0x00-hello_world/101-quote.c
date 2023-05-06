06b86da6ab48ceb717442ef7a5e2b75b6e5da1f4
mideactive <jenyoolumide@yahoo.com>
2023-05-06 13:09:38 +0000

    write a string to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,8 @@
  */
 int main(void)
 {
-	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char *mes = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, mes, 59);
 	return (1);
 }
