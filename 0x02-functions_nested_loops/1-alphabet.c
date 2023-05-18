8d0fcbea1859aa65bc8ef95350e19e78e8beda9f
mideactive <jenyoolumide@yahoo.com>
2023-05-18 19:55:40 +0000

    A function that prints lower alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
  * print_alphabet - Check description
  * Description: It prints the alphabet in lowercase fallowed by a new line
- * Return: Nothing.
+ * Return: Nothing
  */
 void print_alphabet(void)
 {
