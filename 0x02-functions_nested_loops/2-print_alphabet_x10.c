d4bd6a9c5b919fb0906431641209c4f91c8789f3
mideactive <jenyoolumide@yahoo.com>
2023-05-18 20:00:15 +0000

    nexted loop to print lower alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Check holberton
+ * print_alphabet_x10 - Check alx
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
  * Return: Nothing.
  */
-
 void print_alphabet_x10(void)
 {
 	char i;
