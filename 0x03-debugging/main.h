9f3c60397d9282e453a1fa061140877862b7d349
mideactive <jenyoolumide@yahoo.com>
2023-06-02 11:30:52 +0000

    header files

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,10 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void positive_or_negative(int i);
 int largest_number(int, int, int);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
-
 #endif
