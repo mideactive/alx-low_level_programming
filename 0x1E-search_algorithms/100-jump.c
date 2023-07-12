2d54bbf3fae3a6227814eb1d505c0a377e55914a
mideactive <jenyoolumide@yahoo.com>
2023-07-12 10:44:29 +0000

    using jump search to search for values

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -9,37 +9,67 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i;
-	size_t step;
-	size_t jump = sqrt(size);
-	size_t prev = 0;
+	size_t jump_size, i, target_idx;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return (-1);
 
-	while (array[prev] < value)
+	/*find jump size*/
+	jump_size = sqrt(size);
+
+	/*find jump block containing target value*/
+	i = 0;
+	while (i < size && array[i] < value)
 	{
-		step = jump;
-		while (step < size && array[step] <= value)
+		printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
+		if (array[i] == value)
 		{
-			printf("Value checked array[%lu] = [%d]\n", step, array[step]);
-			step += jump;
+			printf("Value found between indexes [%d] and [%d]\n",
+			       (int)(i - jump_size), (int)i);
+			return (i);
 		}
-
-		if (step >= size)
-			break;
-
-		prev += jump;
+		i = i + jump_size;
 	}
 
-		printf("Value found between indexes [%lu] and [%lu]\n", prev, prev + jump);
-
-	for (i = prev; i < size && array[i] <= value; i++)
+	/*perform linear search in block*/
+	if (i >= size || array[i] >= value)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		printf("Value found between indexes [%d] and [%d]\n",
+		       (int)(i - jump_size), (int)i);
+		target_idx = jump_linear_search(array, i + 1, size,
+						jump_size, value);
+		return (target_idx);
+	}
+	return (-1);
+}
+
+/**
+ * linear_search - searches for a value in an array of integers using the
+ * Linear search algorithm
+ *
+ * @array: a pointer to the first element of the array to search in
+ * @start: index of upper end of jump block + 1
+ * @size: number of elements in array from breakpoint
+ * @jump_size: window size when interating through array
+ * @value: value to search for
+ * Return:  first index where value is located or -1 if value is not present
+ * in array or if array is NULL
+ */
+
+
+int jump_linear_search(int *array, size_t start, size_t size,
+		       size_t jump_size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = start - jump_size - 1; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
-
 	return (-1);
 }
